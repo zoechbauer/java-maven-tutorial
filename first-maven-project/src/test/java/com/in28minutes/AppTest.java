eb4117e49ad5063dc954e2224d629e0df7030549
Zoechbauer <hans.zoechbauer@aon.at>
2020-08-29 14:28:35 +0200

    Maven Basics: Change Unit Test for Testing Maven Build Life Cyle

--- first-maven-project/src/test/java/com/in28minutes/AppTest.java
+++ first-maven-project/src/test/java/com/in28minutes/AppTest.java
@@ -1,14 +1,11 @@
 package com.in28minutes;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class AppTest {
 
   @Test
   public void test() {
-    fail("Not yet implemented");
   }
 
 }
